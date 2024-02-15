5d0307adefbeaed31f7b156421702830954d59f0
luisivmaraz <luisivmaraz03@gmail.com>
2024-02-15 17:01:17 -0600

    Giteado chido

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+
+import express from 'express';
+import morgan from 'morgan';
+import {config} from 'dotenv';
+import productsRouter from './routes/products.routes.js'
+config();
+// import {PORT} from './config.js'
+const app = express();
+//setings
+app.set('view engine', 'ejs');
+//Mindlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev')); //morgan usa perfiles el perfil que estamos usando es de desarrollo
+app.use('/api/products', productsRouter);//
+export default app;
