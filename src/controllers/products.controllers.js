acda895e8f44deff6ea20bc1bbfb403549cbc61d
Luis Márquez <141973599+luisivmaraz@users.noreply.github.com>
2024-02-15 17:49:45 -0600

    Update products.controllers.js

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -43,7 +43,7 @@ export const updateOne = (req, res) => {
     ProductDAO.updateOne(barcode, product)
         .then(result => {
             if (result) {
-                res.redirect("/api/products/");
+                res.redirect("/");
             } else {
                 res.json({
                     status: "Product not found"
@@ -60,7 +60,7 @@ export const deleteOne = (req, res) => {
     ProductDAO.deleteOne(barcode)
       .then(result => {
         if (result) {
-            res.redirect("/api/products/");
+            res.redirect("/");
     } else {
         res.json({
             status: "Product not found"
