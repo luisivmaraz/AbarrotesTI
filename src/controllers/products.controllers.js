45f9e76eed3cf55edaf4c9e15f65889fec7ac0e1
Luis Márquez <141973599+luisivmaraz@users.noreply.github.com>
2024-02-19 16:15:53 -0600

    Update products.controllers.js

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -43,7 +43,7 @@ export const updateOne = (req, res) => {
     ProductDAO.updateOne(barcode, product)
         .then(result => {
             if (result) {
-                res.redirect("/");
+                 res.render('../src/views/index.ejs');
             } else {
                 res.json({
                     status: "Product not found"
